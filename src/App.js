57f96581d094c891624c614983090d3598d88ee1
LeanLaxamana28 <leanlaxamana35@gmail.com>
2025-11-13 19:26:02 -0800

    Updated portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      
+      <header className="bg-dark text-white py-3">
+        <div className="container text-center">
+          <h2>Welcome to my Portfolio</h2>
+          <nav>
+            <a href="#about" className="text-white mx-2">About</a>
+            <a href="#projects" className="text-white mx-2">Projects</a>
+            <a href="#contact" className="text-white mx-2">Contact</a>
+          </nav>
+        </div>
       </header>
+<br></br>
+           <br></br>
+            <br></br>
+             <br></br>
+      {/* Main Content */}
+      <main className="container text-center mt-5">
+        <h1>Lean's Portfolio</h1>
+        <p className="lead">
+          This is my personal portfolio.
+        </p> <br></br>
+        <button className="btn btn-success">Click Me.</button>
+      </main>
+<br></br>
+           <br></br>
+            <br></br>
+             <br></br>
+      {/* Footer */}
+      <footer className="bg-dark text-white py-3 mt-5">
+        <div className="container text-center">
+          <br></br>
+          
+          <p>&copy; {new Date().getFullYear()} Lean Laxamana. All rights reserved.</p>
+          <p>
+            <a href="https://github.com/LeanLaxamana28" className="text-white mx-2">GitHub</a> 
+           
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
